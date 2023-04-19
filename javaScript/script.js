fe4deaa138e0e792fd7793e9209ffb45fce24053
Isaac Villa <isaacvillaluna@gmail.com>
2023-04-18 20:03:40 -0600

    meeting window creation

--- javaScript/script.js
+++ javaScript/script.js
@@ -13,9 +13,9 @@ function ocultarVentana() {
   fondo.style.display = "none";
 }
 
-var Bases = ["BASES DE DATOS", "<br>NRC: 52785", "<br>LUIS SANDOVAL HERNÁNDEZ <br>Organizador"];
-var Inglés = ["INGLÉS", "<br>NRC: 57814", "<br>JOSE RAMIREZ ALFARO <br>Organizador"];
-var Álgebra = ["ÁLGEBRA", "<br>NRC: 87642", "<br>ROBERTO ROBLEDO OLIVÉ <br>Organizador"];
-var MineríaDD = ["MINERIA DE DATOS", "<br>NRC: 97842", "<br>MAXIMILIANO TUR LARA <br>Organizador"];
-var ProgramaciónII = ["PROGRAMACION II", "<br>NRC: 21378", "<br>ERNESTO VIDAL PALACIOS <br>Organizador"];
+var Bases = ["BASES DE DATOS<br><br>NRC: 52785<br><br>LUIS SANDOVAL HERNÁNDEZ <br>Organizador <br><br> <a href=reunion.html><span></span><span></span><span></span><span></span><input type=submit value=Iniciar_Reunion></a>"];
+var Inglés = ["INGLÉS", "<br><br>NRC: 57814", "<br><br>JOSE RAMIREZ ALFARO <br>Organizador <br><br> <a href=reunion.html><span></span><span></span><span></span><span></span><input type=submit value=Iniciar_Reunion></a>"];
+var Álgebra = ["ÁLGEBRA", "<br><br>NRC: 87642", "<br><br>ROBERTO ROBLEDO OLIVÉ <br>Organizador <br><br> <a href=reunion.html><span></span><span></span><span></span><span></span><input type=submit value=Iniciar_Reunion></a>"];
+var MineríaDD = ["MINERIA DE DATOS", "<br><br>NRC: 97842", "<br><br>MAXIMILIANO TUR LARA <br>Organizador <br><br> <a href=reunion.html><span></span><span></span><span></span><span></span><input type=submit value=Iniciar_Reunion></a>"];
+var ProgramaciónII = ["PROGRAMACION II", "<br><br>NRC: 21378", "<br><br>ERNESTO VIDAL PALACIOS <br>Organizador <br><br> <a href=reunion.html><span></span><span></span><span></span><span></span><input type=submit value=Iniciar_Reunion></a>"];
 var tabla = document.getElementsByTagName("table")[0];
